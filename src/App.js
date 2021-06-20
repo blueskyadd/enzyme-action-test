6f970a0a26bc19131c83c27897d1b88246261a07
ztj <mars_zhang@yunquna.com>
2021-06-20 12:38:38 +0800

    完成单测例子功能

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { useState, useRef } from "react";
+const Todo = () => {
+    const [list, setList] = useState([
+        { id: 1, item: "Fix bugs" },
+        { id: 2, item: "Take out the trash" }
+    ]);
+    const todoRef = useRef();
+    const removeTodo = id => {
+        setList(list.filter(todo => todo.id !== id));
+    };
+    const addList = data => {
+        let id = list.length + 1;
+        setList([
+            ...list,
+            {
+                id,
+                item: data
+            }
+        ]);
+    };
+    const handleNewTodo = e => {
+        e.preventDefault();
+        const item = todoRef.current;
+        addList(item.value);
+        item.value = "";
+    };
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <h2>Add Todo</h2>
+                </div>
+            </div>
+            <form>
+                <div className="row">
+                    <div className="col-md-6">
+                        <input
+                            type="text"
+                            autoFocus
+                            ref={todoRef}
+                            placeholder="Enter a task"
+                            className="form-control"
+                            data-testid="input"
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <button
+                            type="submit"
+                            onClick={handleNewTodo}
+                            className="btn btn-primary"
+                        >
+                        Add Task
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <div className="row todo-list">
+                <div className="col-md-6">
+                    <h3>Lists</h3>
+                    {!list.length ? (
+                        <div className="no-task">No task!</div>
+                    ) : (
+                        <ul data-testid="list">
+                            {list.map(todo => {
+                                return (
+                                    <li key={todo.id}>
+                                        <div>
+                                            <span>{todo.item}</span>
+                                            <button
+                                                className="btn btn-danger"
+                                                data-testid="delete-button"
+                                                onClick={() => removeTodo(todo.id)}
+                                            >
+                                                删除
+                                            </button>
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Todo;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import 'jsdom-global/register';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Todo", () => {
+  //将一个组件渲染成虚拟DOM对象，但是只渲染第一层，不渲染所有子组件，所以处理速度非常快
+    it("组件渲染", () => {
+        shallow(<App />);
+    }) 
+    it("查询li个数", () => {
+      const wrapper = mount(<App />);
+      expect(wrapper.find("li")).toHaveLength(3);
+    });
+    it("调用addList", () => {
+      const wrapper = mount(<App />);
+      wrapper.find("input").instance().value = "新增加一个";
+      expect(wrapper.find("input").instance().value).toEqual("新增加一个");
+      wrapper.find('[type="submit"]').simulate("click");
+      expect(wrapper.find("li")).toHaveLength(3);
+      expect(wrapper.find("li div span").last().text()).toEqual("新增加一个");
+    });
+
+    it("调用removeTodo", () => {
+      const wrapper = mount(<App />);
+      wrapper.find("li button").first().simulate('click');
+      expect(wrapper.find("li")).toHaveLength(1);
+      expect(wrapper.find("li div span").last().text()).toEqual("Take out the trash");
+    })
 });
